91d3ba0d843664d019defc01825aedf9aa5a258d
svoum-cpu <sevoumvourakis@wm.edu>
2026-03-04 01:35:36 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This is your live URL
+  site: 'https://svoum-cpu.github.io',
+  
+  // Since your repo is exactly svoum-cpu.github.io, 
+  // your 'base' is just the root '/'.
+  base: '/',
+});
